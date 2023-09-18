0e022f56f6f0745acae34cd7d99df3da15bf31ee
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-18 18:15:48 -0500

    homepage work

--- src/components/HeaderComponents/HeaderLink.jsx
+++ src/components/HeaderComponents/HeaderLink.jsx
@@ -4,6 +4,10 @@
 export default function HeaderLink({link, text}){
 
   return (
-    <a href={link} className='hover:bg-violet-100 rounded-lg border border-transparent hover:border-violet-200 py-1 px-2'>{text}</a>
+    <a 
+    href={link} 
+    className='hover:bg-violet-100 rounded-lg border border-transparent hover:border-violet-200 py-1 px-2'>
+      {text}
+    </a>
   )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,35 @@
-
+import zeldaBg from '../assets/zelda-bg.jpg'
+import {
+  SiPlaystation,
+  SiXbox,
+  SiNintendoswitch,
+  SiSteam
+} from 'react-icons/si'
 
 
 export default function Homepage() {
 
   return (
-    <div>
+    <div 
+      style={{
+      backgroundImage: `url(${zeldaBg})`, 
+      backgroundRepeat: 'no-repeat', 
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center',
+      filter: 'grayscale(80%)'
+      }}
+      className='h-[800px] flex flex-col justify-start py-48 items-center'
+      >
+      <h1 className='text-5xl text-violet-900 font-bold p-2'>Game World</h1>
+
+      <div className='flex text-3xl gap-4 text-violet-200'>
+        <SiPlaystation />
+        <SiXbox />
+        <SiNintendoswitch />
+        <SiSteam />
+
+      </div>
+
 
     </div>
   )
