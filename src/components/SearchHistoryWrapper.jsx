7f92caf969b2de8515a238ca714a67a670510d80
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-21 17:10:15 -0500

    added favicon

--- src/components/SearchHistoryWrapper.jsx
+++ src/components/SearchHistoryWrapper.jsx
@@ -3,7 +3,7 @@ import { SearchHistoryLink } from './index'
 
 export default function SearchHistoryWrapper({searchHistory, searchForGames, setInputData}) {
 
-let componentsArr = []
+  let componentsArr = []
   for (let i = 0; i < 5; i++) {
     componentsArr.push(<SearchHistoryLink search={searchHistory[i]} searchForGames={searchForGames} setInputData={setInputData} key={i}/>)
   }
